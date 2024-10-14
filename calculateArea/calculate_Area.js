6e2b9e4aaffc6124db264855ad143b23fab55861
Cesar Torres <cesarreyestorres005@gmail.com>
2024-10-14 10:48:25 -0400

    Practice Task java function

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,8 @@ function calculateArea() {
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker(){
+    let amount = parseFloat(document.getElementById('grocery1').value) + parseFloat(document.getElementById('grocery1').value) + parseFloat(document.getElementById('grocery1').value);
+    document.getElementById('result2').innerHTML = `The total amount is: $${amount}`
+}
