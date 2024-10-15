00dc3ebc1efbf2af3d2cf2e4567ddcd454c70f9a
Cesar Torres <cesarreyestorres005@gmail.com>
2024-10-14 22:38:44 -0400

    Task - User feedback

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,15 +1,17 @@
 const submitButton=document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
 
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
+let experiences = [];
 
 function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    const ux = document.getElementById('userExperience').value;
     alert('Thank you for your valuable feedback')
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -19,6 +21,19 @@ function submitFeedback() {
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
     document.getElementById('userInfo').style.display = 'block';
+
+    experiences.push(ux);
+
+    const mainContainer = document.getElementById('uxs');
+    mainContainer.innerHTML="";
+    for(let i = 0; i < experiences.length; i++){
+        let div = document.createElement("div");
+        div.innerHTML = `<p>${experiences[i]}</p>`;
+        div.style.backgroundColor = "lightblue";
+        div.style.padding = "10px";
+        div.style.margin = "5px 0";
+        mainContainer.appendChild(div);
+    }
 }
 
 document.addEventListener('keydown', function(event) {
